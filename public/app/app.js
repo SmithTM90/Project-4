07a6b8bd5e8475cd7ced0db11ffdad05a60ba44a
Travis Smith <smithtm90@gmail.com>
2016-08-09 15:46:05 -0700

    updated info well for each api return

--- public/app/app.js
+++ public/app/app.js
@@ -11,29 +11,50 @@ bandFinder = angular.module('bandFinder', [])
 
 //   $locationProvider.html5Mode(true);
 // });
-
-bandFinder.controller('finder', ['$scope', '$http', function($scope,$http) {
+bandFinder.controller('finder', ['$scope', '$http', '$sce', function($scope,$http, $sce) {
 
   $scope.search = function(searchTerm) {
     $scope.term = searchTerm
     tasteKid()
   }
 
+  $scope.convertSongUrl = function(songUrl) {
+    return $sce.trustAsResourceUrl(songUrl);
+  }
+
   function tasteKid() {
-    $http.get('https://www.tastekid.com/api/similar?', {
+    $http.get('https://tastekid.com/api/similar?', {
       params: {
         q: $scope.term,
         type: 'music',
         info: 1,
-        limit: 20,
+        limit: 2,
         k:'235626-Project4-CE4XY65G'
       }
     })
     .then(function success(response) {
-      $scope.results = response.data
+      $scope.results = response.data.Similar.Results
+      console.log($scope.results)
+      $scope.results.forEach(function(result, index) {
+        itunesSearch(result.Name, index);
+      });
     }, function error(response) {
-      console.log(response);
+      console.log('tasteKid error');
     })
   }
 
+  function itunesSearch(artist, resultIndex) {
+    $http.get('https://itunes.apple.com/search?', {
+      params: {
+        term: artist,
+        limit: 5
+      }
+    })
+    .then(function success(results) {
+      $scope.results[resultIndex].itunesSongs = results.data.results;
+      console.log($scope.results[resultIndex].itunesSongs);
+    }, function error(results) {
+      console.log(results)
+    })
+  }
 }])
